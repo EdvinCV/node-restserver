332a8df44b276c77e4d124952aa42a53e198bacc
Edvin Calderón <edvinc97@gmail.com>
2020-07-15 21:58:40 -0600

    Commit REST Server Node.js

---
+++ server/server.js
@@ -0,0 +1,46 @@
+// Archivo de configuración
+require('./config/config');
+
+const express = require('express');
+// EXPRESS
+const app = express();
+// Body-Parser
+const bodyParser = require('body-parser');
+
+// body-parser application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extend: false }))
+// parse application/json
+app.use(bodyParser.json());
+
+
+app.get('/usuario', (req, res) => {
+    res.json("get usuario");
+});
+
+app.post('/usuario', (req, res) => {
+    let body = req.body;
+
+    if(body.nombre === undefined){
+        res.status(400).json({
+            ok: false,
+            message: "The field name is required"
+        })
+    } else {
+        res.json(body);
+    }
+});
+
+app.put('/usuario/:id', (req, res) => {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', (req, res) => {
+    res.json("delete usuario");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Escuchando server...");
+});
